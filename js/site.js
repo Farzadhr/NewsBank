4650edb6ad9beaec4d3d6b088b751c75078d47ac
Farzad Heydari <fheydari207@gmail.com>
2024-04-30 11:25:55 +0430

    update get image and fix it

--- js/site.js
+++ js/site.js
@@ -14,54 +14,80 @@ $("#41fa8bce").html(men.format('YYYY/MM/DD'));
 $("#243f75ca").html(men.format('ddd - DD MMMM YYYY'));
 
 $("#OpenMobileMenu").click(function (e) {
-    e.preventDefault();
-    $("#MobileMenu").removeClass("-right-full");
-    $("#MobileMenu").addClass("right-0");
+  e.preventDefault();
+  $("#MobileMenu").removeClass("-right-full");
+  $("#MobileMenu").addClass("right-0");
 });
 $("#CloseMobileMenu").click(function (e) {
-    e.preventDefault();
-    $("#MobileMenu").addClass("-right-full");
-    $("#MobileMenu").removeClass("right-0");
+  e.preventDefault();
+  $("#MobileMenu").addClass("-right-full");
+  $("#MobileMenu").removeClass("right-0");
 });
+
+var normalNews = JSON.parse(normjson);
+for (var i of normalNews) {
+  var address = i["Address"]
+  var data = `
+  <div class="w-full flex justify-center">
+  <div class="w-9/12 rounded-sm">
+      <img src="${address}" class="w-full" alt="">
+  </div>
+</div>
+  `
+  document.getElementById("owlNormalNews").innerHTML += data
+}
+var sportNews = JSON.parse(sportjson)
+for (var i of sportNews) {
+  var address = i["Address"]
+  var data = `
+  <div class="w-full flex justify-center">
+  <div class="w-9/12 rounded-sm">
+      <img src="${address}" class="w-full" alt="">
+  </div>
+</div>
+  `
+  document.getElementById("owlSportNews").innerHTML += data
+}
+
+
+
+
+
 
 var owlnormal = $('.owl-carousel-normal')
 
 owlnormal.owlCarousel({
-    autoplay: false,
-    autoplayTimeout: 7000,
-    autoplayHoverPause: false,
-    loop:true,
-    rtl: true,
-    margin: 10,
-    nav: true,
-    items: 1,
-    dots: false,
-    center: true,
-    navContainerClass: "owl-nav flex justify-between absolute top-1/2 right-[3%] w-[94%]",
-    navText: ['<p class="bg-primary text-white h-8 w-8 rounded-full flex items-center justify-center"><span class="mingcute--right-line h-6 w-6"></span></p>', '<p class="bg-primary text-white h-8 w-8 rounded-full flex items-center justify-center"><span class="mingcute--left-line h-6 w-6"></span></p>']
+  autoplay: false,
+  autoplayTimeout: 7000,
+  autoplayHoverPause: false,
+  loop: true,
+  rtl: true,
+  margin: 10,
+  nav: true,
+  items: 1,
+  dots: false,
+  center: true,
+  navContainerClass: "owl-nav flex justify-between absolute top-1/2 right-[3%] w-[94%]",
+  navText: ['<p class="bg-primary text-white h-8 w-8 rounded-full flex items-center justify-center"><span class="mingcute--right-line h-6 w-6"></span></p>', '<p class="bg-primary text-white h-8 w-8 rounded-full flex items-center justify-center"><span class="mingcute--left-line h-6 w-6"></span></p>']
 })
-owlnormal.trigger('play.owl.autoplay',[7000])
+owlnormal.trigger('play.owl.autoplay', [7000])
 
 var owlsport = $('.owl-carousel-sport')
 
 owlsport.owlCarousel({
-    autoplay: false,
-    autoplayTimeout: 7000,
-    autoplayHoverPause: false,
-    loop:true,
-    rtl: true,
-    margin: 10,
-    nav: true,
-    items: 1,
-    dots: false,
-    center: true,
-    navContainerClass: "owl-nav flex justify-between absolute top-1/2 right-[3%] w-[94%]",
-    navText: ['<p class="bg-primary text-white h-8 w-8 rounded-full flex items-center justify-center"><span class="mingcute--right-line h-6 w-6"></span></p>', '<p class="bg-primary text-white h-8 w-8 rounded-full flex items-center justify-center"><span class="mingcute--left-line h-6 w-6"></span></p>']
+  autoplay: false,
+  autoplayTimeout: 7000,
+  autoplayHoverPause: false,
+  loop: true,
+  rtl: true,
+  margin: 10,
+  nav: true,
+  items: 1,
+  dots: false,
+  center: true,
+  navContainerClass: "owl-nav flex justify-between absolute top-1/2 right-[3%] w-[94%]",
+  navText: ['<p class="bg-primary text-white h-8 w-8 rounded-full flex items-center justify-center"><span class="mingcute--right-line h-6 w-6"></span></p>', '<p class="bg-primary text-white h-8 w-8 rounded-full flex items-center justify-center"><span class="mingcute--left-line h-6 w-6"></span></p>']
 })
-owlsport.trigger('play.owl.autoplay',[7000])
+owlsport.trigger('play.owl.autoplay', [7000])
 
-fetch("https://farzadhr.ir/NewsNormal.json")
-.then(res => res.json())
-.then(out =>
-  console.log('Checkout this JSON! ', out))
-.catch(err => { throw err });
+
